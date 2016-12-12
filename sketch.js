7baf4a2da6fd89c9a05be31551823168d28bd3ab
Marcus <zxmarcus@gmail.com>
2016-12-11 17:48:06 -0800

    Implemented a bougie pause function yeeeee

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ var food;
 
 // Still needs work
 // https://forum.processing.org/two/discussion/5644/how-to-make-a-pause-screen
-var stateGame = 0;
-var statePause = 1;
-var state = statePause;
+var gameState = 0;
+var pauseState = 1;
+var state = pauseState;
 
 //var cols = floor(width/scl);
 //var rows = floor(height/scl);
@@ -37,11 +37,11 @@ function mousePressed(){
 		if (gameState === true) {
 			
 			// Pause
-			gameState = false;
+			state = pauseState;
 			console.log('Game Paused!');
 		
 		} else if (gameState === false) {
-			gameState = true;
+			sate = gameState;
 			console.log('Game Unpaused')
 		}
 	}
@@ -49,6 +49,29 @@ function mousePressed(){
 }
 
 function draw(){
+	// Runs on and on
+
+	switch(state){
+		case gameState:
+			// Playing
+			playGame();
+			break;
+
+		case pauseState:
+			//Paused
+			pauseGame();
+			break;
+
+		default:
+			// default block, results in error
+			console.log("Error ocurred; unknown state: " + state + ".");
+			break;
+	}
+}
+
+function playGame(){
+	// Game
+	//handleStateGame();
 
 	background(51);
 
@@ -64,10 +87,36 @@ function draw(){
 
 	fill(255, 0, 100);
 	rect(food.x, food.y, scl, scl);
-	
+}
+
+function pauseGame(){
+	//background(200);
+	textSize(28);
+	text("Game paused! Press p to resume.");
 }
 
 function keyPressed(){
+
+	switch(state){
+		case gameState:
+		// Playing
+		keyPressedGameState();
+		break;
+
+		case pauseState:
+		// Paused
+		keyPressedPauseState();
+		break;
+
+		default:
+		// Error
+		console.log("Error ocurred; Unknown sate: " + state + ".");
+		break;
+	}
+}
+
+function keyPressedGameState(){
+
 	if (keyCode === UP_ARROW/* || 'w'*/) {
 		snake.dir(0, -1);
 	} else if (keyCode === DOWN_ARROW) {
@@ -76,6 +125,19 @@ function keyPressed(){
 		snake.dir(-1, 0);
 	} else if (keyCode === RIGHT_ARROW) {
 		snake.dir(1, 0);
+	} else if (key === 'P' || key === 'p') {
+		console.log("Game paused!");
+		state = pauseState;
+	}
+}
+
+function keyPressedPauseState(){
+	if (key === 'P' || key === 'p'){
+		console.log("Game unpaused!");
+		state = gameState;
+	} else {
+		console.log("Button input in pause state.");
+		state = pauseState;
 	}
 }
 
